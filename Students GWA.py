b9adf6372e3ae2584239138f1f5dca3a8d73214e
unknown <rv8828120@gmail.com>
2023-04-26 23:17:01 +0800

    Open txt file in read mode

--- Students GWA.py
+++ Students GWA.py
@@ -3,6 +3,7 @@
 #Object-Oriented Programming
 
 #Open the txt file in read mode
+with open("studentsGWA.txt", "r") as raw_files:
     #Loop through each line in the file
         #Split the line into student name and GWA, then convert the GWA to float
         #Check if the current GWA is lower than the current lowest GWA
