537c22a29cac781c5e3b7e0da7d55b9520986db1
unknown <rv8828120@gmail.com>
2023-04-26 23:24:44 +0800

    Create an empty list to hold the name(s) of the student(s) with the highest GWA

--- Students GWA.py
+++ Students GWA.py
@@ -4,6 +4,8 @@
 
 #Set the initial lowest GWA value to 5.0
 lowest_GWA = 5.0
+#Create an empty list to hold the name(s) of the student(s) with the highest GWA
+highest_student = []
 #Open the txt file in read mode
 with open("studentsGWA.txt", "r") as raw_files:
     #Loop through each line in the file
