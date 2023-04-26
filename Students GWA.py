93dfda56abfd4cd70f9b0008b133b38869aa30da
unknown <rv8828120@gmail.com>
2023-04-26 23:18:57 +0800

    Loop through each line in the file

--- Students GWA.py
+++ Students GWA.py
@@ -5,6 +5,7 @@
 #Open the txt file in read mode
 with open("studentsGWA.txt", "r") as raw_files:
     #Loop through each line in the file
+    for line in raw_files:
         #Split the line into student name and GWA, then convert the GWA to float
         #Check if the current GWA is lower than the current lowest GWA
     #Print the Outcome
