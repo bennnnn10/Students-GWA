9691f581419c354e27152535ee6b1b97d84989e6
unknown <rv8828120@gmail.com>
2023-04-26 23:12:04 +0800

    Pseudocode

--- Students GWA.py
+++ Students GWA.py
@@ -1,3 +1,9 @@
 #Vargas, Ruben A.
 #BSCPE 1-4
 #Object-Oriented Programming
+
+#Open the txt file in read mode
+    #Loop through each line in the file
+        #Split the line into student name and GWA, then convert the GWA to float
+        #Check if the current GWA is lower than the current lowest GWA
+    #Print the Outcome
